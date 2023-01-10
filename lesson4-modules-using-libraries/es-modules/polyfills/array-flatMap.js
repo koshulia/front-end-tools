85e0f379480fcdb0b53dcbb4a997ea906f827492
Uliana Koshuba <koshubaulyana@gmail.com>
2023-01-10 05:12:39 +0200

    commit

---
+++ lesson4-modules-using-libraries/es-modules/polyfills/array-flatMap.js
@@ -0,0 +1,9 @@
+// здесь никакие экспорты не нужны
+
+console.log("polyfill for flatMap");
+
+window.Array.prototype.flatMap =
+  Array.prototype.flatMap ||
+  function flatMap() {
+    // ... implementation for older browsers
+  };
